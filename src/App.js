125bfa6ca9474b57a176f32baae30ce783192796
Haard Shah <haard2014@outlook.com>
2024-09-03 17:57:11 +0200

    Added delete icons to FeedbackItem

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import FeedbackData from "./data/feedbackData"
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const deleteFeedbackItem = (delId) => {
+        if(window.confirm('Are you sure you want to delete the item?')) {
+            setFeedback(
+                feedback.filter((item) => item.id !== delId)
+            )
+        }
+    }
+
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackList feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedbackItem} />
             </div>
         </>
     )
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,21 +1,19 @@
 import FeedbackItem from "./FeedbackItem"
 import PropTypes from 'prop-types'
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
 
-    if (!feedback || feedback.lenghth === 0) {
-        return <p>No Feedback Yet.</p>
+    if (!feedback || feedback.length === 0) {
+      return <p>No Feedback Yet.</p>
     }
 
-  return (
-    <div className="feedback-list">
-        {
-            feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item}  />
-            ))
-        }
-    </div>
-  )
+    return (
+      <div className="feedback-list">
+        {feedback.map((item) => (
+          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+        ))}
+      </div>
+    )
 }
 
 FeedbackList.propTypes = {
